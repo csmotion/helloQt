78cd040479f33245276e2c86cdb53b7c9f3d121d
csmotion <jim.schwendeman@gmail.com>
2022-02-25 18:23:54 -0700

    Update helloQt.py

--- helloQt.py
+++ helloQt.py
@@ -1,24 +1,7 @@
 import sys
 import os
 
-# from PySide2.QtWidgets import QApplication
-# os.environ['QT_PREFERRED_BINDING'] = 'PySide2'
 from Qt import (QtWidgets, __binding__, __binding_version__, __qt_version__, __version__)
-
-# os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = r'~qgis directory\apps\Qt5\plugins'
-# os.environ['PATH'] += r';~qgis directory\apps\qgis\bin;~qgis directory\apps\Qt5\bin'
-
-# from PyQt5.QtCore import QLibraryInfo
-# from PySide2.QtCore import QLibraryInfo
-
-# os.environ["QT_QPA_PLATFORM_PLUGIN_PATH"] = "C:\PROJECTS\Rocky Mountain Servo, LLC\SW\Python\Toy Problems\PyInstaller Qt Shim\myvenv\Lib\site-packages\PySide2\plugins"
-# from pyqtgraph.Qt import QtGui
-
-# pyinstaller -F helloQT.py --paths=C:\python37\Lib\site-packages\ --hidden-import=Qt.py --debug=imports
-# pyinstaller -F -c helloQT.py --paths=C:\python37\Lib\site-packages\ --hidden-import=Qt.py --debug=imports
-# pyinstaller helloQt.py helloQT.spec --windowed
-
-# pyinstaller --onefile --hidden-import=Qt.py --hidden-import=PySide2.QtXml --hidden-import=PySide2.QtUiTools helloQt.py helloQT.spec --windowed
 
 def window():
     app = QtWidgets.QApplication(sys.argv)
