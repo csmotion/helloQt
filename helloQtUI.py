b6c6308bfe4ea0ecb5fdb361746ed49bd76550d3
csmotion <jim.schwendeman@gmail.com>
2022-03-10 14:16:34 -0700

    Add files via upload
    JRS - commit changes to allow helloQtUI.py to dynamically import vendored or user supplied PySide2 library.

--- helloQtUI.py
+++ helloQtUI.py
@@ -1,6 +1,31 @@
 import sys
 import os
 
+# pyinstaller --onefile --hidden-import=Qt.py --hidden-import=PySide2.QtXml --hidden-import=PySide2.QtUiTools helloQt.py helloQT.spec --windowed
+# pyinstaller helloQtUI.spec
+
+# Dynamically import PySide2 for LGPL license: https://stackoverflow.com/questions/56615123/python-use-importlib-to-import-a-module-from-a-package-directory/56615264#56615264
+if os.path.isdir('./PySide2/'):
+	print('Importing local PySide2')
+	IMPORTED_FROM = "user supplied import!"
+
+	MODULE_PATH = "./PySide2/__init__.py"
+	MODULE_NAME = "PySide2"
+
+	import importlib.util
+	spec = importlib.util.spec_from_file_location(MODULE_NAME, MODULE_PATH)
+	module = importlib.util.module_from_spec(spec)
+	sys.modules[spec.name] = module
+	spec.loader.exec_module(module)
+	PySide2 = module
+else:
+	print('Importing system PySide2')
+	IMPORTED_FROM = "vendor supplied import!"
+	
+	import PySide2
+	print(PySide2.__version__)
+
+# os.environ['QT_PREFERRED_BINDING'] = 'PySide2'
 from Qt import (QtCompat, QtWidgets, __binding__, __binding_version__, __qt_version__, __version__)
 
 class MainWindow(QtWidgets.QWidget):
@@ -11,11 +36,9 @@ class MainWindow(QtWidgets.QWidget):
 
 		# Import .ui forms for the GUI using function resource_path()
 		form = self.resource_path("GUI\helloQtUI.ui")
+		self.uiLoader(form, self)
 
-		self.baseInst = self.uiLoader(form, self)
-		# self.setFocusPolicy(QtCore.Qt.StrongFocus)
-		# self.keyPressEvent.connect(self.KeyPressEvent)
-		# self.connectSlotsMain()
+		self.lineEdit.setText(IMPORTED_FROM)
 
 	def resource_path(self, relative_path):
 		""" Get absolute path to resource, works for dev and for PyInstaller """
@@ -28,6 +51,21 @@ class MainWindow(QtWidgets.QWidget):
 		return os.path.join(base_path, relative_path)
 
 	def uiLoader(self, uifile, baseInst=None):
+		"""
+		Qt.py/PyQtGraph compatible loading of Qt Designer .ui file, loaded into baseInst
+
+		Parameters
+		----------
+		uifile : .ui
+			QT Designer UI file
+		baseInst : _type_, optional
+			_description_, by default None
+
+		Returns
+		-------
+		_type_
+			_description_
+		"""
 		ui = QtCompat.loadUi(uifile)  # Qt.py mapped function
 		if not baseInst:
 			return ui
